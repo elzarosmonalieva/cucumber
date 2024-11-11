8bf69a53a75fa936de97dea06b3b9f70bafd6142
elzar <elzosman4@gmail.com>
2024-11-11 12:43:29 -0600

    added new messages, made some difference in last-branch

--- src/test/java/pages/checking.java
+++ src/test/java/pages/checking.java
@@ -2,9 +2,10 @@ package pages;
 
 public class checking {
     public static void main(String[] args) {
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i <10 ; i++) {
             System.out.println("Hello folks");
         }
-
+        System.out.println("welcome to codewise!");
+        System.out.println("work harder guys!");
     }
 }
