ddfe33073def902f68d98636d169ee6c4fe1a11e
elzar <elzosman4@gmail.com>
2024-11-11 11:20:42 -0600

    "added new file"

---
+++ src/test/java/pages/checking.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class checking {
+    public static void main(String[] args) {
+        System.out.println("reverse string ");
+        System.out.println("My project");
+        System.out.println("My project");
+        System.out.println("My project");
+        System.out.println("My project");
+        System.out.println("My project");
+
+    }
+}
